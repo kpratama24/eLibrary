be584a95c65c62a80cb1b20736ff542f8ec97d98
Kevin Pratama <Kevin Pratama>
2016-11-20 19:05:33 +0700

    Dependencies.php fixes. Still have issue.

--- modules/dependencies.php
+++ modules/dependencies.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" href="./lib/material.min.css" media="screen" title="no title">
-    <link rel="stylesheet" href="./font-awesome.min.css" media="screen" title="no title">
+<link rel="stylesheet" href="../../lib/w3.css" media="screen" title="no title">
+    <link rel="stylesheet" href="../../lib/font-awesome.min.css" media="screen" title="no title">
     <meta name="viewport" content="width=device-width, initial-scale=1">
