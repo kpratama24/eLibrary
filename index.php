70d084151065b00bff70b297b5e4ea441fdce194
Kevin Pratama <Kevin Pratama>
2016-11-20 19:30:42 +0700

    Add index background. index.php done

--- index.php
+++ index.php
@@ -3,14 +3,35 @@
   <head>
     <meta charset="utf-8">
     <title>eLibrary</title>
-    <?php
-    include './modules/dependencies.php';
-     ?>
+    <link rel="stylesheet" href="./lib/w3.css" media="screen" title="no title">
+    <link rel="stylesheet" href="./lib/font-awesome.min.css" media="screen" title="no title">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
   </head>
+  <style media="screen">
+    body,h1,h5 {font-family: "Raleway", sans-serif}
+    body, html {height: 100%}
+    .bgimg {
+    background-image: url('./img/library.jpg');
+    min-height: 100%;
+    background-position: center;
+    background-size: cover;
+  }
+  </style>
   <body>
-    
+    <div class="bgimg w3-display-container w3-text-white">
+      <div class="w3-display-middle w3-jumbo w3-padding-jumbo">
+        <p>eLibrary</p>
+      </div>
+      <div class="w3-display-topleft w3-padding-jumbo w3-xlarge">
+        <a href="./pages/general/login.php"><button class="w3-btn w3-hover-light-grey"> Login </button></a>
+        <a href="./pages/general/signup.php"><button class="w3-btn w3-hover-light-grey"> Sign Up </button></a>
+      </div>
+      <div class="w3-display-bottomleft w3-padding-jumbo">
+        <p class="w3-xlarge w3-text-black"> Parahyangan Catholic University</p>
+        <p class="w3-large w3-text-black"> Creative Developers </p>
+        <p class="w3-large w3-text-black"> &copy; Prayogo Cendra and Kevin Pratama </p>
+      </div>
+    </div>
   </body>
-  <?php
-  include './modules/jsdependencies.php';
-   ?>
 </html>
--- pages/general/login.php
+++ pages/general/login.php
@@ -12,21 +12,5 @@
 	<br>
 	<input type="submit" value="Login">
 </form>
-<div class="w3-card-4">
-
-<header class="w3-container w3-light-grey">
-  <h3>John Doe</h3>
-</header>
-
-<div class="w3-container">
-  <p>1 new friend request</p>
-  <hr>
-  <img src="img_avatar3.png" alt="Avatar" class="w3-left w3-circle">
-  <p>President/CEO at Mighty Schools...</p>
-</div>
-
-<button class="w3-btn-block w3-dark-grey">+ Connect</button>
-
-</div>
 </body>
 </html>
