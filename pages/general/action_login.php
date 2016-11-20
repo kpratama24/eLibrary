e0d5d5895248886322e7740f78be0e1559ddd07b
yogo-chen <prayogocendra@gmail.com>
2016-11-20 17:12:16 +0700

    Update action_login.php

--- pages/general/action_login.php
+++ pages/general/action_login.php
@@ -1,28 +1,43 @@
 <?php
-if (isset($_POST['username']) && preg_match('/^[ ]*[a-zA-Z0-9_]{6,32}[ ]*$/', $_POST['username'])) {
+if (isset($_POST['username']) && preg_match('/^[ ]*[a-zA-Z0-9_]{4,32}[ ]*$/', $_POST['username'])) {
 	$username = strtolower(trim($_POST['username']));
 } else {
+	header("Location: login.php?error=username");
 	die("Username field doesn't meet criteria");
 }
 if (isset($_POST['password']) && $_POST['password']) {
 	$password = $_POST['password'];
 } else {
+	header("Location: login.php?error=password");
 	die("Password field doesn't meet criteria");
 }
 
 $dbh = include '../../dbh.php';
-$sql = "SELECT password FROM user WHERE username = :username";
+$sql = "SELECT u.id AS id, u.username AS username, u.password AS password, u.name AS name, u.role_id AS role_id, r.role_name AS role_name FROM user AS u JOIN role AS r WHERE u.username = :username AND u.role_id = r.id";
 $params = array(':username' => $username);
 $sth = $dbh->prepare($sql);
 $sth->execute($params);
 
 if ($sth->rowCount()) {
-	if (password_verify($password, $sth->fetch(PDO::FETCH_ASSOC)['password'])) {
-		echo "Valid password";
+	$row = $sth->fetch(PDO::FETCH_ASSOC);
+	if (password_verify($password, $row['password'])) {
+
+		session_destroy();
+		session_start(); 
+		$_SESSION['username'] = $row[username];
+		$_SESSION['name'] = $row[name];
+		$_SESSION['roleId'] = $row[role_id];
+		$_SESSION['roleName'] = $row[role_name];
+		$_SESSION['loggedIn'] = true;
+
+		header("Location: signup.php?error=address");		
+		die("Login success");
 	} else {
+		header("Location: login.php?error=invalid");
 		die("Invalid username or password");
 	}
 } else {
+	header("Location: login.php?error=invalid");
 	die("Invalid username or password");
 }
 ?>
