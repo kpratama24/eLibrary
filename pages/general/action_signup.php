05d8afef0c279a02b308695a2e507bd08cd473a5
yogo-chen <prayogocendra@gmail.com>
2016-11-20 17:12:49 +0700

    Update action_signup.php

--- pages/general/action_signup.php
+++ pages/general/action_signup.php
@@ -1,7 +1,8 @@
 <?php
-if (isset($_POST['username']) && preg_match('/^[ ]*[a-zA-Z0-9_]{6,32}[ ]*$/', $_POST['username'])) {
+if (isset($_POST['username']) && preg_match('/^[ ]*[a-zA-Z0-9_]{4,32}[ ]*$/', $_POST['username'])) {
 	$username = strtolower(trim($_POST['username']));
 } else {
+	header("Location: signup.php?error=username");
 	die("Username field doesn't meet criteria");
 }
 
@@ -12,26 +13,31 @@ $sth = $dbh->prepare($sql);
 $sth->execute($params);
 
 if($sth->rowCount()) {
+	header("Location: signup.php?error=usernametaken");
 	die("Username is already used");
 }
 if (isset($_POST['password']) && $_POST['password']) {
 	$password = $_POST['password'];
 } else {
+	header("Location: signup.php?error=password");
 	die("Password field doesn't meet criteria");
 }
 if (isset($_POST['name']) && preg_match('/^[ ]*[a-zA-Z]+([ ][a-zA-Z]+)*[ ]*$/', $_POST['name'])) {
 	$name = trim($_POST['name']);
 } else {
+	header("Location: signup.php?error=name");
 	die("Name field doesn't meet criteria");
 }
 if (isset($_POST['phone']) && preg_match('/^[ ]*[+]?[0-9]{4,}[ ]*$/', $_POST['phone'])) {
 	$phone = trim($_POST['phone']);
 } else {
+	header("Location: signup.php?error=phone");
 	die("Phone field doesn't meet criteria");
 }
 if (isset($_POST['address']) && $_POST['address']) {
 	$address = trim($_POST['address']);
 } else {
+	header("Location: signup.php?error=address");
 	die("Address field doesn't meet criteria");
 }
 
@@ -44,4 +50,7 @@ $sql = "INSERT INTO user(username, password, name, phone, address, role_id) VALU
 $params = array(':username' => $username, ':password' => $password, ':name' => $name, ':phone' => $phone, ':address' => $address, ':roleId' => $roleId);
 $sth = $dbh->prepare($sql);
 $result = $sth->execute($params);
+
+header("Location: signup.php?error=address");
+die("Signup success");
 ?>
