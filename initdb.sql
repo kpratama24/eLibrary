f93201c81881f67e657ff46c7c10afc4df195cd4
Cecilia Stefiany <Cecilia Stefiany>
2016-11-20 12:27:06 +0700

    Create initdb.sql

---
+++ initdb.sql
@@ -0,0 +1,58 @@
+DROP DATABASE IF EXISTS elibrary;
+
+CREATE DATABASE elibrary;
+
+USE elibrary;
+
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  FOREIGN KEY (`role_id`) REFERENCES `role`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(10) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `publisher` varchar(100) NOT NULL,
+  `year` date NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`category_id`) REFERENCES `category`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `loan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `loan_date` date NOT NULL,
+  `return_date` date DEFAULT NULL,
+  `max_day` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`id`),
+  FOREIGN KEY (`book_id`) REFERENCES `book`(`id`)
+);
+
+INSERT INTO role(role_name) VALUES ('administrator');
+
+INSERT INTO role(role_name) VALUES ('librarian');
