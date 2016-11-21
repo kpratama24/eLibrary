11c7c9567c8a8a4f088057d82305f350be55fb64
unknown <i14033@LAB03-32.ftis.unpar>
2016-11-21 07:57:16 +0700

    Create templates

--- modules/dependencies.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="../../lib/w3.css" media="screen" title="no title">
-    <link rel="stylesheet" href="../../lib/font-awesome.min.css" media="screen" title="no title">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
---
+++ templates/style.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="../css/w3.css">
+<link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="../css/style.css">
