65fc5f8803f601d41e7445b41b3d7f97aaf0964c
Prayogo Cendra <prayogocendra@gmail.com>
2016-11-24 15:52:08 +0700

    Create administrator list for admin

--- pages/admin/admList.php
+++ pages/admin/admList.php
@@ -10,6 +10,38 @@ if (!isset($_SESSION['id'])) {
 
 include '../../templates/header.php';
 ?>
+<div class="w3-card-2 w3-white">
+	<div class="w3-container w3-black">
+		<h2>Administrator List</h2>
+	</div>
+	<table class="w3-table w3-bordered">
+		<tr>
+			<th>Username</th>
+			<th>Name</th>
+			<th>Phone</th>
+			<th>Address</th>
+		</tr>
+<?php
+$dbh = include '../../modules/dbh.php';
+$sql = "SELECT username, name, phone, address FROM user WHERE role_id = :role_id";
+$sth = $dbh->prepare($sql);
+$sth->execute(array(':role_id' => 1));
+
+$users = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($users as $user) {
+?>
+		<tr>
+			<td><?php echo $user['username'] ?></td>
+			<td><?php echo $user['name'] ?></td>
+			<td><?php echo $user['phone'] ?></td>
+			<td><?php echo $user['address'] ?></td>
+		</tr>
+<?php
+}
+?>
+	</table>
+</div>
 <?php
 include '../../templates/footer.php';
 ?>
