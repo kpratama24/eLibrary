9d653cc3069d97f59cbd49f1c8ab164472261b31
yogo-chen <prayogocendra@gmail.com>
2016-11-24 20:45:27 +0700

    Add filter menu

--- pages/admin/member.php
+++ pages/admin/member.php
@@ -9,11 +9,52 @@ if (!isset($_SESSION['id'])) {
 }
 
 include '../../templates/header.php';
+
+$searchCriteria = array(
+	'username' => "AND username LIKE :query",
+	'name' => "AND name LIKE :query",
+	'phone' => "AND phone LIKE :query",
+	'address' => "AND address LIKE :query"
+);
+
+$dbh = include '../../modules/dbh.php';
+$sql = "SELECT username, name, phone, address FROM user WHERE role_id = :role_id";
+if (isset($_GET['option']) && array_key_exists($_GET['option'], $searchCriteria)) {
+	$sql = $sql . " " . $searchCriteria[$_GET['option']];
+	$sth = $dbh->prepare($sql);
+	$sth->execute(array(':role_id' => 2, ':query' => "%" . $_GET['query'] . "%"));
+} else {
+	$sth = $dbh->prepare($sql);
+	$sth->execute(array(':role_id' => 2));
+}
+
+$users = $sth->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <div class="w3-card-2 w3-white">
 	<div class="w3-container w3-black">
 		<h2>Member List</h2>
 	</div>
+	<form class="w3-row-padding" action="member.php" method="get">
+		<div class="w3-col s4 m3 l2 w3-section w3-row-padding">
+			<div class="w3-col" style="width: 50px;">
+				<label for="option-field"><i class="w3-xxlarge fa fa-filter"></i></label>
+			</div>
+			<div class="w3-rest">
+				<select class="w3-select w3-border w3-light-grey" name="option" id="option-field">
+					<option value="username" <?php echo (isset($_GET['option']) && $_GET['option'] == 'username') ? 'selected' : ''; ?>>Username</option>
+					<option value="name" <?php echo (isset($_GET['option']) && $_GET['option'] == 'name') ? 'selected' : ''; ?>>Name</option>
+					<option value="phone" <?php echo (isset($_GET['option']) && $_GET['option'] == 'phone') ? 'selected' : ''; ?>>Phone</option>
+					<option value="address" <?php echo (isset($_GET['option']) && $_GET['option'] == 'address') ? 'selected' : ''; ?>>Address</option>
+				</select>
+			</div>
+		</div>
+		<div class="w3-col s5 m7 l8 w3-section">
+			<input type="text" name="query" placeholder="Search Term" class="w3-input w3-light-grey" value=<?php echo isset($_GET['query']) ? $_GET['query'] : ''; ?>>
+		</div>
+		<div class="w3-col s3 m2 l2 w3-section">
+			<input type="submit" value="Search" class="w3-btn-block">
+		</div>
+	</form>
 	<table class="w3-table w3-bordered">
 		<tr>
 			<th>Username</th>
@@ -22,13 +63,6 @@ include '../../templates/header.php';
 			<th>Address</th>
 		</tr>
 <?php
-$dbh = include '../../modules/dbh.php';
-$sql = "SELECT username, name, phone, address FROM user WHERE role_id = :role_id";
-$sth = $dbh->prepare($sql);
-$sth->execute(array(':role_id' => 2));
-
-$users = $sth->fetchAll(PDO::FETCH_ASSOC);
-
 foreach ($users as $user) {
 ?>
 		<tr>
