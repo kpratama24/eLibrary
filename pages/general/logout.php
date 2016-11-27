ce345bc0064e629d4430772c637f6974ccec7d2b
Kevin Pratama <kevin@Kevins-iMac.local>
2016-11-27 23:10:35 +0700

    Shorten logout redirect time to 3 seconds

--- pages/general/logout.php
+++ pages/general/logout.php
@@ -6,13 +6,13 @@ session_destroy();
 
 include '../../templates/header.php';
 ?>
-<meta http-equiv="refresh" content="5; url=../">
+<meta http-equiv="refresh" content="3; url=../">
 <div class="w3-card-2 w3-white">
 	<div class="w3-container w3-brown">
 		<h2>You have successfully logged out</h2>
 	</div>
-	<div class="w3-container">	
-		<p>You will be redirected in 5 seconds</p>
+	<div class="w3-container">
+		<p>You will be redirected in 3 seconds</p>
 	</div>
 </div>
 <?php
